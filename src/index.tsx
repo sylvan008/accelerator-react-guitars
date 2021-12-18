a21fa49d110e79f17dc75a576b2bc75b31d4ad2a
sylvan008 <sylvan.climb@gmail.com>
2021-12-19 00:07:08 +0300

    Подключает store к приложению

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
+import {createApi} from './services/api';
+import {rootReducer} from './store/root-reducer';
+import {loadGuitars} from './store/api-action';
 import App from './components/app/app';
+
+const api = createApi();
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    thunk: {
+      extraArgument: api,
+    },
+  }),
+});
+
+store.dispatch(loadGuitars());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root'));
+  document.getElementById('root'),
+);
