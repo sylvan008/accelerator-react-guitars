5cf8c698366437c59d367dea1d43d21357d1ce1c
sylvan008 <sylvan.climb@gmail.com>
2021-12-18 21:46:32 +0300

    добавляет переадресаю с main на catalog

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,4 @@
-import {Route, Router} from 'react-router-dom';
+import {Redirect, Route, Router} from 'react-router-dom';
 import {browserHistory} from '../../services/browser-history';
 import {AppRoute} from '../../utils/const/app-route';
 import PageCatalog from '../page-catalog/page-catalog';
@@ -6,6 +6,7 @@ import PageCatalog from '../page-catalog/page-catalog';
 function App(): JSX.Element {
   return (
     <Router history={browserHistory}>
+      <Redirect from="/" to={AppRoute.Catalog} />
       <Route exact path={AppRoute.Catalog}>
         <PageCatalog />
       </Route>
