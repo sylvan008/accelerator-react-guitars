b6c841a7e240a614ed56c97366736c884fbd13ac
sylvan008 <sylvan.climb@gmail.com>
2021-12-19 18:35:35 +0300

    Добавляет вывод результата поиска по названию гитары

---
+++ src/components/search-result-list/search-result-list.tsx
@@ -0,0 +1,19 @@
+import {Guitar} from '../../types/guitar';
+
+type PropsType = {
+  searchResult: Guitar[],
+}
+
+function SearchResultList(props: PropsType): JSX.Element {
+  const {searchResult} = props;
+
+  return (
+    <ul className="form-search__select-list">
+      {searchResult.map((item) => (
+        <li className="form-search__select-item" tabIndex={0} key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default SearchResultList;
--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,24 +1,51 @@
+import {
+  ChangeEvent,
+  FormEvent,
+  useEffect,
+  useState
+} from 'react';
+import {useSelector} from 'react-redux';
+import {getGuitars} from '../../store/catalog-process/selectors';
+import {Guitar} from '../../types/guitar';
+import {findGuitars} from '../../utils/utils';
+import SearchResultList from '../search-result-list/search-result-list';
+
 function Search(): JSX.Element {
+  const guitars = useSelector(getGuitars);
+  const [searchString, setSearchString] = useState('');
+  const [foundedGuitars, setFoundedGuitars] = useState<Guitar[]>([]);
+
+  const onSearchInputChange = (evt: ChangeEvent<HTMLInputElement>) => setSearchString(evt.target.value);
+  const onSearchFormSubmit = (evt: FormEvent) => evt.preventDefault();
+
+  useEffect(() => {
+    setFoundedGuitars(findGuitars(guitars, searchString));
+  }, [searchString, guitars]);
+
   return (
     <div className="form-search">
-      <form className="form-search__form">
+      <form
+        className="form-search__form"
+        onSubmit={onSearchFormSubmit}
+      >
         <button className="form-search__submit" type="submit">
           <svg className="form-search__icon" width="14" height="15" aria-hidden="true">
             <use xlinkHref="#icon-search" />
           </svg>
           <span className="visually-hidden">Начать поиск</span>
         </button>
-        <input className="form-search__input" id="search" type="text" autoComplete="off" placeholder="что вы ищите?" />
+        <input
+          className="form-search__input"
+          id="search"
+          type="text"
+          autoComplete="off"
+          placeholder="что вы ищите?"
+          value={searchString}
+          onChange={onSearchInputChange}
+        />
         <label className="visually-hidden" htmlFor="search">Поиск</label>
       </form>
-      <ul className="form-search__select-list hidden">
-        <li className="form-search__select-item" tabIndex={0}>Четстер Plus</li>
-        <li className="form-search__select-item" tabIndex={0}>Четстер UX</li>
-        <li className="form-search__select-item" tabIndex={0}>Четстер UX2</li>
-        <li className="form-search__select-item" tabIndex={0}>Четстер UX3</li>
-        <li className="form-search__select-item" tabIndex={0}>Четстер UX4</li>
-        <li className="form-search__select-item" tabIndex={0}>Четстер UX5</li>
-      </ul>
+      {searchString.length !== 0 && <SearchResultList searchResult={foundedGuitars} />}
     </div>
   );
 }
