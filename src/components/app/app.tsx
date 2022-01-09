4be31c9dec8fa3a689fac1cf93884139c0b7a76e
sylvan008 <sylvan.climb@gmail.com>
2022-01-09 20:31:24 +0300

    Исправляет редирект на страницу каталога

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -4,12 +4,11 @@ import {AppRoute} from '../../utils/const/app-route';
 import PageCatalog from '../page-catalog/page-catalog';
 
 function App(): JSX.Element {
+  const {search} = browserHistory.location;
   return (
     <Router history={browserHistory}>
-      <Redirect from="/" to={AppRoute.Catalog} />
-      <Route exact path={AppRoute.Catalog}>
-        <PageCatalog />
-      </Route>
+      <Redirect exact from="/" to={`${AppRoute.Catalog}${search.toString()}`} />
+      <Route exact path={AppRoute.Catalog} component={PageCatalog} />
     </Router>
   );
 }
