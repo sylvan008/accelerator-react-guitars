1f94e1677f1daa6441e01da1c8fd12eda0518299
sylvan008 <sylvan.climb@gmail.com>
2022-01-24 18:00:45 +0300

    Добавляет тест для корректного рендера CatalogFilter.

---
+++ src/components/catalog-filter/catalog-filter.test.tsx
@@ -0,0 +1,31 @@
+import {render, screen} from '@testing-library/react';
+import CatalogFilter from './catalog-filter';
+import {PriceBounds} from '../../types/store';
+import {GuitarKind, stringsCounts} from '../../utils/const/filter';
+
+describe('Component: CatalogFilter', () => {
+  const priceBounds = [0, 0] as PriceBounds;
+  const priceMinSearch = priceBounds[0];
+  const priceMaxSearch = priceBounds[1];
+  const searchGuitarString = [stringsCounts[0]];
+  const searchGuitarTypes = [GuitarKind.Acoustic];
+
+  const setSearchParamsMock = jest.fn();
+
+  it('should render correctly', () => {
+    const filterRegexp = /фильтр/i;
+
+    render(
+      <CatalogFilter
+        priceBounds={priceBounds}
+        priceMinSearch={priceMinSearch}
+        priceMaxSearch={priceMaxSearch}
+        searchGuitarString={searchGuitarString}
+        searchGuitarTypes={searchGuitarTypes}
+        setSearchParams={setSearchParamsMock}
+      />,
+    );
+
+    expect(screen.getByText(filterRegexp)).toBeInTheDocument();
+  });
+});
