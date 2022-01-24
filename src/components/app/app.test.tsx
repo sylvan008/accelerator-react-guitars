48746a7fe1a0cda17c12776bce05aa6102559560
sylvan008 <sylvan.climb@gmail.com>
2022-01-24 15:12:21 +0300

    Добавляет тест роутинга

--- src/components/app/app.test.tsx
+++ src/components/app/app.test.tsx
@@ -1,8 +1,44 @@
+import thunk, {ThunkDispatch} from 'redux-thunk';
 import {render, screen} from '@testing-library/react';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {Action} from '@reduxjs/toolkit';
+import {createMemoryHistory} from 'history';
+import {Provider} from 'react-redux';
+import {Router} from 'react-router-dom';
+import {NameSpace, State} from '../../store/root-reducer';
 import App from './app';
 
-test('Renders app-component', () => {
-  render(<App />);
-  const textElement = screen.getByText(/Hello, world!/i);
-  expect(textElement).toBeInTheDocument();
+const middlewares = [thunk];
+const mockStore = configureMockStore<
+  State,
+  Action,
+  ThunkDispatch<State, unknown, Action>
+>(middlewares);
+
+const store = mockStore({
+  [NameSpace.Catalog as string]: {
+    guitars: [],
+    priceBounds: [0, 0],
+    isCatalogLoad: true,
+  },
+});
+
+const history = createMemoryHistory();
+
+const fakeApp = (
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>
+);
+
+describe('App routing', () => {
+  it('should render catalog page', () => {
+    const catalogRegexp = /каталог гитар/i;
+
+    render(fakeApp);
+
+    expect(screen.getByText(catalogRegexp)).toBeInTheDocument();
+  });
 });
