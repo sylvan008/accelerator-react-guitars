9df70ef25b2f7d2fb027294f134a726485433a0f
sylvan008 <sylvan.climb@gmail.com>
2022-02-15 17:30:36 +0300

    Добавляет запрос данных с сервера.

--- src/components/page-product.tsx
+++ src/components/page-product.tsx
@@ -1,8 +1,45 @@
+import {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {loadGuitar} from '../store/api-action';
+import {useParams} from 'react-router-dom';
+import Loader from './loader/loader';
+import {ThunkAppDispatch} from '../types/actionType';
+import {getGuitar} from '../store/catalog-process/selectors';
+import {Guitar} from '../types/guitar';
+import {formatPrice} from '../utils/format';
+import {replaceImagePath} from '../utils/utils';
 import MainLayout from './layouts/main-layout/main-layout';
 import Breadcrumbs from './breadcrumbs/breadcrumbs';
 import ProductRating from './product-rating/product-rating';
 
+type PageParams = {
+  id: string,
+};
+
 function PageProduct(): JSX.Element {
+  const {id: guitarId}: PageParams = useParams();
+  const guitar: Guitar | null = useSelector(getGuitar);
+  const dispatch = useDispatch();
+
+  useEffect( () => {
+    (dispatch as ThunkAppDispatch)(loadGuitar(Number(guitarId)));
+  }, [guitarId, dispatch]);
+
+  if (!guitar) {
+    return <Loader />;
+  }
+
+  const {
+    description,
+    name,
+    rating,
+    price,
+    previewImg,
+    stringCount,
+    type,
+    vendorCode,
+  } = guitar;
+
   return (
     <MainLayout>
       <main className="page-content">
@@ -10,10 +47,16 @@ function PageProduct(): JSX.Element {
           <h1 className="page-content__title title title--bigger">Товар</h1>
           <Breadcrumbs />
           <div className="product-container">
-            <img className="product-container__img" src="/img/content/guitar-2.jpg" width="90" height="235" alt="" />
+            <img
+              className="product-container__img"
+              src={replaceImagePath(previewImg)}
+              width="90"
+              height="235"
+              alt={`гитара ${name}`}
+            />
             <div className="product-container__info-wrapper">
-              <h2 className="product-container__title title title--big title--uppercase">СURT Z30 Plus</h2>
-              <ProductRating rating={4} />
+              <h2 className="product-container__title title title--big title--uppercase">{name}</h2>
+              <ProductRating rating={rating} />
               <div className="tabs">
                 <a className="button button--medium tabs__button" href="#characteristics">Характеристики</a>
                 <a className="button button--black-border button--medium tabs__button" href="#description">Описание</a>
@@ -21,29 +64,28 @@ function PageProduct(): JSX.Element {
                   <table className="tabs__table">
                     <tr className="tabs__table-row">
                       <td className="tabs__title">Артикул:</td>
-                      <td className="tabs__value">SO754565</td>
+                      <td className="tabs__value">{vendorCode}</td>
                     </tr>
                     <tr className="tabs__table-row">
                       <td className="tabs__title">Тип:</td>
-                      <td className="tabs__value">Электрогитара</td>
+                      <td className="tabs__value">{type}</td>
                     </tr>
                     <tr className="tabs__table-row">
                       <td className="tabs__title">Количество струн:</td>
-                      <td className="tabs__value">6 струнная</td>
+                      <td className="tabs__value">{stringCount}</td>
                     </tr>
                   </table>
                   <p className="tabs__product-description hidden">
-                    Гитара подходит как для старта обучения, так и для
-                    домашних занятий или использования в полевых условиях, например, в походах или для проведения уличных
-                    выступлений. Доступная стоимость, качество и надежная конструкция, а также приятный внешний вид,
-                    который сделает вас звездой вечеринки.
+                    {description}
                   </p>
                 </div>
               </div>
             </div>
             <div className="product-container__price-wrapper">
               <p className="product-container__price-info product-container__price-info--title">Цена:</p>
-              <p className="product-container__price-info product-container__price-info--value">52 000 ₽</p>
+              <p className="product-container__price-info product-container__price-info--value">
+                {formatPrice(price)}
+              </p>
               <a className="button button--red button--big product-container__button" href="#">Добавить в корзину</a>
             </div>
           </div>
