69e0848cbdd0e44bccf52f6a87ac8e082efc6724
sylvan008 <sylvan.climb@gmail.com>
2022-02-18 14:58:19 +0300

    Выводит комментарии полученные с сервера.

--- src/components/page-product/page-product.tsx
+++ src/components/page-product/page-product.tsx
@@ -1,20 +1,21 @@
 import {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
-import {loadGuitar} from '../../store/api-action';
+import {loadComments, loadGuitar} from '../../store/api-action';
 import Loader from '../loader/loader';
 import {ThunkAppDispatch} from '../../types/actionType';
-import {getGuitar} from '../../store/catalog-process/selectors';
+import {getComments, getGuitar} from '../../store/product-process/selectors';
 import {Guitar} from '../../types/guitar';
 import {formatPrice} from '../../utils/format';
-import {replaceImagePath} from '../../utils/utils';
 import {ProductTab} from '../../utils/const/product-tabs';
+import {replaceImagePath} from '../../utils/utils';
 import MainLayout from '../layouts/main-layout/main-layout';
 import Breadcrumbs from '../breadcrumbs/breadcrumbs';
 import ProductRating from '../product-rating/product-rating';
 import Tabs from '../tabs/tabs';
 import TabCharacteristic from '../tab-characteristic/tab-characteristic';
 import TabDescription from '../tab-description/tab-description';
+import Reviews from '../reviews/reviews';
 
 type PageParams = {
   id: string,
@@ -23,12 +24,15 @@ type PageParams = {
 function PageProduct(): JSX.Element {
   const {id: guitarId}: PageParams = useParams();
   const guitar: Guitar | null = useSelector(getGuitar);
+  const comments = useSelector(getComments);
   const dispatch = useDispatch();
   const [isDataLoaded, setIsDataLoaded] = useState(false);
 
   useEffect( () => {
+    const guitarIdString =  Number(guitarId);
     setIsDataLoaded(false);
-    (dispatch as ThunkAppDispatch)(loadGuitar(Number(guitarId)))
+    (dispatch as ThunkAppDispatch)(loadGuitar(guitarIdString))
+      .then(() => (dispatch as ThunkAppDispatch)(loadComments(guitarIdString)))
       .then(() => setIsDataLoaded(true));
   }, [guitarId, dispatch]);
 
@@ -86,118 +90,7 @@ function PageProduct(): JSX.Element {
               <a className="button button--red button--big product-container__button" href="#">Добавить в корзину</a>
             </div>
           </div>
-          <section className="reviews">
-            <h3 className="reviews__title title title--bigger">Отзывы</h3>
-            <a className="button button--red-border button--big reviews__sumbit-button" href="#">Оставить отзыв</a>
-            <div className="review">
-              <div className="review__wrapper">
-                <h4 className="review__title review__title--author title title--lesser">Иванов Максим</h4>
-                <span className="review__date">12 декабря</span>
-              </div>
-              <div className="rate review__rating-panel" aria-hidden="true">
-                <span className="visually-hidden">Рейтинг:</span>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-full-star" />
-                </svg>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-full-star" />
-                </svg>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-full-star" />
-                </svg>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-full-star" />
-                </svg>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-star" />
-                </svg>
-                <span className="rate__count" />
-                <span className="rate__message" />
-              </div>
-              <h4 className="review__title title title--lesser">Достоинства:</h4>
-              <p className="review__value">Хороший корпус, чистый звук, стурны хорошего качества</p>
-              <h4 className="review__title title title--lesser">Недостатки:</h4>
-              <p className="review__value">Тугие колонки</p>
-              <h4 className="review__title title title--lesser">Комментарий:</h4>
-              <p className="review__value">
-                У гитары отличный цвет, хороше дерево. Тяжелая, в компдлекте неть чехла и
-                ремня.
-              </p>
-            </div>
-            <div className="review">
-              <div className="review__wrapper">
-                <h4 className="review__title review__title--author title title--lesser">Перова Ольга</h4>
-                <span className="review__date">12 декабря</span>
-              </div>
-              <div className="rate review__rating-panel" aria-hidden="true">
-                <span className="visually-hidden">Рейтинг:</span>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-full-star" />
-                </svg>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-full-star" />
-                </svg>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-full-star" />
-                </svg>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-full-star" />
-                </svg>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-star" />
-                </svg>
-                <span className="rate__count" />
-                <span className="rate__message" />
-              </div>
-              <h4 className="review__title title title--lesser">Достоинства:</h4>
-              <p className="review__value">Хороший корпус, чистый звук, стурны хорошего качества</p>
-              <h4 className="review__title title title--lesser">Недостатки:</h4>
-              <p className="review__value">Тугие колонки</p>
-              <h4 className="review__title title title--lesser">Комментарий:</h4>
-              <p className="review__value">
-                У гитары отличный цвет, хороше дерево. Тяжелая, в компдлекте неть чехла и ремня.
-              </p>
-            </div>
-            <div className="review">
-              <div className="review__wrapper">
-                <h4 className="review__title review__title--author title title--lesser">Преображенская Ксения</h4>
-                <span className="review__date">12 декабря</span>
-              </div>
-              <div className="rate review__rating-panel" aria-hidden="true">
-                <span className="visually-hidden">Рейтинг:</span>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-full-star" />
-                </svg>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-full-star" />
-                </svg>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-full-star" />
-                </svg>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-full-star" />
-                </svg>
-                <svg width="16" height="16" aria-hidden="true">
-                  <use xlinkHref="#icon-star" />
-                </svg>
-                <span className="rate__count" />
-                <span className="rate__message" />
-              </div>
-              <h4 className="review__title title title--lesser">Достоинства:</h4>
-              <p className="review__value">Хороший корпус, чистый звук, стурны хорошего качества</p>
-              <h4 className="review__title title title--lesser">Недостатки:</h4>
-              <p className="review__value">Тугие колонки</p>
-              <h4 className="review__title title title--lesser">Комментарий:</h4>
-              <p className="review__value">
-                У гитары отличный цвет, хороше дерево. Тяжелая, в компдлекте неть чехла и
-                ремня. У гитары отличный цвет, хороше дерево. Тяжелая, в компдлекте неть чехла и ремня. У гитары отличный
-                цвет, хороше дерево. Тяжелая, в компдлекте неть чехла и ремня. У гитары отличный цвет, хороше дерево.
-                Тяжелая, в компдлекте неть чехла и ремня.
-              </p>
-            </div>
-            <button className="button button--medium reviews__more-button">Показать еще отзывы</button>
-            <a className="button button--up button--red-border button--big reviews__up-button" href="#header">Наверх</a>
-          </section>
+          <Reviews comments={comments} />
         </div>
       </main>
     </MainLayout>
