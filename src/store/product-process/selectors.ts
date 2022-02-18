e69fc4ff5dc67d22b417a480f1e50d32e11dca2e
sylvan008 <sylvan.climb@gmail.com>
2022-02-18 14:57:05 +0300

    Рефакторит селектор комментариев. Адаптирует комментария для клиента и сортирует их по дате.

--- src/store/product-process/selectors.ts
+++ src/store/product-process/selectors.ts
@@ -1,7 +1,10 @@
 import {NameSpace, State} from '../root-reducer';
+import {adaptCommentToClient, sortingCommentsByDate} from '../../utils/utils';
 
 const getGuitar = (state: State) => state[NameSpace.Product].guitar;
-const getComments = (state: State) => state[NameSpace.Product].comments;
+const getComments = (state: State) => state[NameSpace.Product].comments
+  .map(adaptCommentToClient)
+  .sort(sortingCommentsByDate);
 
 export {
   getGuitar,
