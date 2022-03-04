278f0d41f0cde1b5e92d4029918b5340530fcf5b
sylvan008 <sylvan.climb@gmail.com>
2022-03-04 15:05:23 +0300

    Добавит в тесты Router

--- src/components/search-result-list/search-result-list.test.tsx
+++ src/components/search-result-list/search-result-list.test.tsx
@@ -1,6 +1,8 @@
 import {render, screen} from '@testing-library/react';
 import SearchResultList from './search-result-list';
 import {createMockGuitar} from '../../utils/mock/guitar-mock';
+import {Router} from 'react-router-dom';
+import {createMemoryHistory} from 'history';
 
 describe('Component: SearchResultList', () => {
   const searchResult = [
@@ -11,9 +13,16 @@ describe('Component: SearchResultList', () => {
   const nameRegexp = new RegExp(searchResult.map((guitar) => guitar.name).join('|'), 'i');
 
   const onResultClick = jest.fn();
+  const history = createMemoryHistory();
+
+  const fakeApp = (
+    <Router history={history}>
+      <SearchResultList searchResult={searchResult} onResultClick={onResultClick} />
+    </Router>
+  );
 
   it('should render correctly', () => {
-    render(<SearchResultList searchResult={searchResult} onResultClick={onResultClick} />);
+    render(fakeApp);
 
     expect(screen.getAllByText(nameRegexp)).toHaveLength(searchResult.length);
   });
