2b345ac1d92a6d65ef73779d84cbc4c9087d099e
sylvan008 <sylvan.climb@gmail.com>
2022-03-05 18:27:10 +0300

    Исправит интерактивную область Logo в футере

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,9 +1,15 @@
 import {Link} from 'react-router-dom';
 import {AppRoute} from '../../utils/const/app-route';
 
-function Logo(): JSX.Element {
+type PropsType = {
+  className?: string,
+};
+
+function Logo(props: PropsType): JSX.Element {
+  const {className} = props;
+  const classes = ['logo', className ? className : ''].join(' ').trim();
   return (
-    <Link className="header__logo logo" to={AppRoute.Main}>
+    <Link className={classes} to={AppRoute.Main}>
       <img className="logo__img" width="70" height="70" src="/img/svg/logo.svg" alt="Логотип" />
     </Link>
   );
