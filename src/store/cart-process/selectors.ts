6d50a39ddac1f9077c3a2b52f43250d8b5f4fbb2
sylvan008 <sylvan.climb@gmail.com>
2022-03-12 17:36:43 +0300

    Добавляет состояние корзины

--- src/store/cart-process/selectors.ts
+++ src/store/cart-process/selectors.ts
@@ -1,7 +1,8 @@
 import {NameSpace, State} from '../root-reducer';
 
 const getCartItems = (state: State) => state[NameSpace.Cart].items;
-const getCartItemsCount = (state: State) => state[NameSpace.Cart].items.length;
+const getCartItemsCount = (state: State) => state[NameSpace.Cart].items
+  .reduce((reducer, item) => reducer + item.count, 0);
 
 export {
   getCartItems,
