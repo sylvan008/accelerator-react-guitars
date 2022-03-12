cb16776be42e5168f2aace9d7fabb67bdefe1b66
sylvan008 <sylvan.climb@gmail.com>
2022-03-12 18:33:25 +0300

    Добавляет состояние корзины

--- src/components/shopping-cart/shopping-cart.tsx
+++ src/components/shopping-cart/shopping-cart.tsx
@@ -5,14 +5,14 @@ import {getCartItemsCount} from '../../store/cart-process/selectors';
 
 function ShoppingCart(): JSX.Element {
   const cartItemsCount = useSelector(getCartItemsCount);
-
+  const isCountShow = cartItemsCount > 0;
   return (
     <Link className="header__cart-link" to={AppRoute.Cart} aria-label="Корзина">
       <svg className="header__cart-icon" width="14" height="14" aria-hidden="true">
         <use xlinkHref="#icon-basket" />
       </svg>
       <span className="visually-hidden">Перейти в корзину</span>
-      <span className="header__cart-count">{cartItemsCount}</span>
+      {isCountShow && <span className="header__cart-count">{cartItemsCount}</span>}
     </Link>
   );
 }
