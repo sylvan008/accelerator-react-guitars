f8f2ceac943b4643133f5083dec5f542f3224c21
sylvan008 <sylvan.climb@gmail.com>
2022-03-13 09:49:50 +0300

    Добавит словарь переводов для типов гитар

--- src/utils/const/locale.ts
+++ src/utils/const/locale.ts
@@ -1,3 +1,9 @@
 export const Locale = ({
   RU: 'ru-RU',
 } as const);
+
+export const GuitarDictionary = {
+  'acoustic': 'Акустическая',
+  'electric': 'Электрогитара',
+  'ukulele': 'Укулеле',
+};
