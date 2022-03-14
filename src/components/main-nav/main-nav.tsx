a61ab55e3800a61d3ca3e1a37c4de34916ec7128
sylvan008 <sylvan.climb@gmail.com>
2022-03-14 18:28:22 +0300

    Исправит ссылку в шапке сайта

--- src/components/main-nav/main-nav.tsx
+++ src/components/main-nav/main-nav.tsx
@@ -1,13 +1,31 @@
-import {Link} from 'react-router-dom';
+import {Link, matchPath, useLocation} from 'react-router-dom';
 import {AppRoute} from '../../utils/const/app-route';
 import {TestId} from '../../utils/const/test-id';
 
+function checkPath(pathname: string, route: string, exact: boolean): boolean {
+  const match = matchPath(
+    pathname,
+    {
+      path: route,
+      exact: false,
+    },
+  );
+  return route === match?.path && match.isExact === exact;
+}
+
 function MainNav(): JSX.Element {
+  const {pathname} = useLocation();
+  const isCatalogPage = checkPath(pathname, AppRoute.CatalogPage, true);
+  const isCatalog = checkPath(pathname, AppRoute.Catalog, true);
+
+  const classNames = ['link', 'main-nav__link', isCatalogPage || isCatalog ? 'link--current' : '']
+    .join(' ').trim();
+
   return (
     <nav className="main-nav" data-testid={TestId.MainNav}>
       <ul className="main-nav__list">
         <li>
-          <Link className="link main-nav__link link--current" to={AppRoute.Catalog}>Каталог</Link>
+          <Link className={classNames} to={AppRoute.Catalog}>Каталог</Link>
         </li>
         <li>
           <a className="link main-nav__link" href="#">Где купить?</a>
